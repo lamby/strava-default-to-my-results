591f4e466986528f3cedc61d8952e8139b9411e9
Chris Lamb <chris@chris-lamb.co.uk>
2014-02-02 12:36:55 +0000

    Reflow.

--- js/injected.js
+++ js/injected.js
@@ -2,9 +2,9 @@ var view = Strava.Labs.Activities.SegmentLeaderboardView;
 var fn = view.prototype.render;
 
 view.prototype.render = function () {
+  var elem = jQuery(this.el);
   var result = fn.apply(this, Array.prototype.slice.call(arguments));
 
-  var elem = jQuery(this.el);
   if (!elem.hasClass('disabled')) {
     elem.addClass('disabled').find('.clickable[data-filter=my_results]').click();
   }
