f619910b923bbf8bd688b474aad3eb1e88b22172
Chris Lamb <chris@chris-lamb.co.uk>
2014-03-07 23:10:41 +0000

    Run all this privately.

--- js/injected.js
+++ js/injected.js
@@ -1,22 +1,24 @@
-var view = Strava.Labs.Activities.SegmentLeaderboardView;
-var fn = view.prototype.render;
+(function() {
+  var view = Strava.Labs.Activities.SegmentLeaderboardView;
+  var fn = view.prototype.render;
 
-view.prototype.render = function () {
-  var elem = jQuery(this.el);
-  var result = fn.apply(this, Array.prototype.slice.call(arguments));
+  view.prototype.render = function () {
+    var elem = jQuery(this.el);
+    var result = fn.apply(this, Array.prototype.slice.call(arguments));
 
-  if (elem.hasClass('once-only')) {
+    if (elem.hasClass('once-only')) {
+      return result;
+    }
+
+    // Only perform logic once per dropdown to avoid infinite loop.
+    elem.addClass('once-only');
+
+    // Only show "My Results" if we have at least one, which we can infer from
+    // whether we have a PR listed.
+    if (elem.find('.pr-comparison time').last().text().trim() !== "s") {
+      elem.find('.clickable[data-filter=my_results]').click();
+    }
+
     return result;
-  }
-
-  // Only perform logic once per dropdown to avoid infinite loop.
-  elem.addClass('once-only');
-
-  // Only show "My Results" if we have at least one, which we can infer from
-  // whether we have a PR listed.
-  if (elem.find('.pr-comparison time').last().text().trim() !== "s") {
-    elem.find('.clickable[data-filter=my_results]').click();
-  }
-
-  return result;
-};
+  };
+})();
